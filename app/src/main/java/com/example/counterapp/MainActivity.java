acc5dccf89923e8d3c6fd10aa74d5d1cfa94e5e0
SushobhanPal <sushobhanpalofficial@gmail.com>
2026-01-27 23:44:35 +0530

    Phase 1 of the project done

--- app/src/main/java/com/example/counterapp/MainActivity.java
+++ app/src/main/java/com/example/counterapp/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.counterapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    ConstraintLayout cl;
+    private int countvar=0;
+TextView countText;
+Button resetbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +26,18 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        cl=findViewById(R.id.main);
+        countText=findViewById(R.id.countTxt);
+        resetbtn=findViewById(R.id.resetBtn);;
+
+        cl.setOnClickListener(v->{
+            countvar++;
+            countText.setText(String.valueOf(countvar));
+        });
+        resetbtn.setOnClickListener(v -> {
+                    countvar=0;
+                    countText.setText(String.valueOf(countvar));
+                }
+        );
     }
 }
